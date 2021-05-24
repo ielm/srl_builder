97591f637b58da85b014e77f8526f8be506d19a5
Ivan E. Leon <leoni@panacea.cogsci.rpi.edu>
2021-05-24 14:52:10 -0400

    write file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from allennlp.predictors.predictor import Predictor
 
 import allennlp_models.tagging
 import json
+import time
 
 from pprint import pprint
 
@@ -18,16 +19,19 @@ def extract_deps(text: str, dep_predictor):
     return dep_predictor.predict(sentence=text)
 
 if __name__ == '__main__':
-    with open('data/Persuasion/svo_dialogs.json', 'r', encoding='utf8') as file:
+    with open('/remote/ayuser/leoni/gator_data/Persuasion/svo_dialogs.json', 'r', encoding='utf8') as file:
         data = [d for d in json.loads(file.read()) if not "BAD" in d['dialog_id']]
 
     srl_predictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/structured-prediction-srl-bert.2020.12.15.tar.gz")
     dep_predictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/biaffine-dependency-parser-ptb-2020.04.06.tar.gz")
-    
-    for dialog in data:
-        process_dialog(dialog, srl_predictor=srl_predictor, dep_predictor=dep_predictor)
 
-    with open('data/Persuasion/srl_dialogs', 'r', encoding='utf8') as wfile:
+    for dialog in data:
+	print(f"\nProcessing dialog: {dialog['dialog_id']}")
+	start_time = time.time()
+        process_dialog(dialog, srl_predictor=srl_predictor, dep_predictor=dep_predictor)
+	print(f"Dialog Execution Time: {time.time()-start_time}")
+
+    with open('/remote/ayuser/leoni/gator_data/Persuasion/srl_dialogs.json', 'w', encoding='utf8') as wfile:
         wfile.write(json.dumps(data))
 
 
