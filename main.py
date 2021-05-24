1dbc74970a6ee89a8a136ce1c1f076cd361c1ced
ielm <leoni@rpi.edu>
2021-05-24 12:02:20 -0400

    extract semantic role labels and dependency relations

---
+++ main.py
@@ -0,0 +1,36 @@
+from allennlp.predictors.predictor import Predictor
+
+import allennlp_models.tagging
+import json
+
+from pprint import pprint
+
+def process_dialog(dialog: dict, srl_predictor: Predictor, dep_predictor: Predictor):
+    for turn in dialog['turns']:
+        turn["srl"] = extract_srl(turn['text'], srl_predictor)
+        turn["deps"] = extract_deps(turn['text'], dep_predictor)
+    return dialog
+
+def extract_srl(text: str, srl_predictor):
+    return srl_predictor.predict(sentence=text)
+
+def extract_deps(text: str, dep_predictor):
+    return dep_predictor.predict(sentence=text)
+
+if __name__ == '__main__':
+    with open('data/Persuasion/svo_dialogs.json', 'r', encoding='utf8') as file:
+        data = [d for d in json.loads(file.read()) if not "BAD" in d['dialog_id']]
+
+    srl_predictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/structured-prediction-srl-bert.2020.12.15.tar.gz")
+    dep_predictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/biaffine-dependency-parser-ptb-2020.04.06.tar.gz")
+    
+    for dialog in data:
+        process_dialog(dialog, srl_predictor=srl_predictor, dep_predictor=dep_predictor)
+
+    with open('data/Persuasion/srl_dialogs', 'r', encoding='utf8') as wfile:
+        wfile.write(json.dumps(data))
+
+
+
+
+
